6620e645f09fc6c3e8380258798c50acbe8e6ca0
sivsanka <siva29042003@gmail.com>
2024-10-16 20:07:24 +0530

    Demo java 5th line added by siva

--- Demo.java
+++ Demo.java
@@ -3,6 +3,6 @@ class Demo
   public static void main(String []args){
     System.out.println("Java Everywhere");
     System.out.println("Java Still Worth To learn");
-    System.out.println("Learn Java ");
+    System.out.println("Learn Java");
   }
 }
